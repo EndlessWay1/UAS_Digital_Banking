da4db39ab6f4a2b8ce6ec9700fb88b4bea06dfa0
Denzel Mark Wahyudi <Denzelmacbook@Denzels-Macbook.local>
2026-05-26 23:46:20 +0700

    feat: build prototype for cardless transactions

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CardlessTransactionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/deposit', [CardlessTransactionController::class, 'deposit']);
+Route::post('/deposit', [CardlessTransactionController::class, 'withdraw']);
