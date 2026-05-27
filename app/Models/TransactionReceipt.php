bbfcd41c6b72039ee0f0d984030a54aac0fff556
vaibhav0629 <kapadiavaibhav19@gmail.com>
2026-05-28 03:55:18 +0700

    Membuat model Transaction dan TransactionReceipt

--- app/Models/TransactionReceipt.php
+++ app/Models/TransactionReceipt.php
@@ -1,10 +1,16 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class TransactionReceipt extends Model
 {
-    //
+    protected $fillable = [
+        'transaction_id',
+        'receipt_number',
+    ];
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class);
+    }
 }
