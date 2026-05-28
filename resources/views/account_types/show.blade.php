059ba7fcc0f23be7b898177f6b117a9f7ccc0289
Daffa Prawira <daffaprawira04@gmail.com>
2026-05-28 13:59:25 +0700

    Add account balance and account type views

---
+++ resources/views/account_types/show.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Account Type Detail</title>
+</head>
+<body>
+    <h1>Account Type Detail</h1>
+
+    <p>
+        <a href="{{ route('account-types.index') }}">Back to Account Types</a>
+    </p>
+
+    <table border="1" cellpadding="8">
+        <tr>
+            <th>Name</th>
+            <td>{{ $accountType->name }}</td>
+        </tr>
+        <tr>
+            <th>Code</th>
+            <td>{{ $accountType->code }}</td>
+        </tr>
+        <tr>
+            <th>Description</th>
+            <td>{{ $accountType->description }}</td>
+        </tr>
+        <tr>
+            <th>Minimum Balance</th>
+            <td>Rp {{ number_format($accountType->minimum_balance, 0, ',', '.') }}</td>
+        </tr>
+    </table>
+</body>
+</html>
