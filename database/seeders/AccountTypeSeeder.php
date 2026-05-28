1f6e1759389b68ccb4144c657eab1122692f5302
Daffa Prawira <daffaprawira04@gmail.com>
2026-05-28 13:33:46 +0700

    Fix account type dropdown

--- database/seeders/AccountTypeSeeder.php
+++ database/seeders/AccountTypeSeeder.php
@@ -2,16 +2,39 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\AccountType;
 use Illuminate\Database\Seeder;
 
 class AccountTypeSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $types = [
+            [
+                'name' => 'Savings',
+                'code' => 'savings',
+                'description' => 'Rekening tabungan untuk transaksi harian.',
+                'minimum_balance' => 0,
+            ],
+            [
+                'name' => 'Investment',
+                'code' => 'investment',
+                'description' => 'Rekening untuk kebutuhan investasi.',
+                'minimum_balance' => 100000,
+            ],
+            [
+                'name' => 'Business',
+                'code' => 'business',
+                'description' => 'Rekening untuk kebutuhan bisnis.',
+                'minimum_balance' => 500000,
+            ],
+        ];
+
+        foreach ($types as $type) {
+            AccountType::updateOrCreate(
+                ['code' => $type['code']],
+                $type
+            );
+        }
     }
 }
