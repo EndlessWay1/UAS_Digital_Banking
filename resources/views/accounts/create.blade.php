97391cf72bc3c166136a1dd0eb55f79a56afa208
Daffa Prawira <daffaprawira04@gmail.com>
2026-06-10 10:07:05 +0700

    Fix initial balance account creation

--- resources/views/accounts/create.blade.php
+++ resources/views/accounts/create.blade.php
@@ -7,11 +7,13 @@
     <h1>Create New Account</h1>
 
     <p>
+        <a href="{{ route('home') }}">Back to Home</a> |
         <a href="{{ route('accounts.index') }}">Back to Accounts</a>
     </p>
 
     @if ($errors->any())
         <div style="color: red;">
+            <strong>There are validation errors:</strong>
             <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
@@ -27,9 +29,10 @@
             <label>Account Type</label><br>
             <select name="account_type_id" required>
                 <option value="">-- Select Account Type --</option>
+
                 @foreach ($accountTypes as $type)
                     <option value="{{ $type->id }}" {{ old('account_type_id') == $type->id ? 'selected' : '' }}>
-                        {{ $type->name }} - Min Balance Rp {{ number_format($type->minimum_balance, 0, ',', '.') }}
+                        {{ $type->name }} - Minimum Balance Rp {{ number_format($type->minimum_balance, 0, ',', '.') }}
                     </option>
                 @endforeach
             </select>
@@ -38,8 +41,24 @@
         <br>
 
         <div>
+            <label>Initial Balance</label><br>
+            <input
+                type="number"
+                name="initial_balance"
+                min="0"
+                value="{{ old('initial_balance', 0) }}"
+                required
+            >
+            <br>
+            <small>Initial balance must follow the selected account type minimum balance.</small>
+        </div>
+
+        <br>
+
+        <div>
             <label>PIN</label><br>
             <input type="password" name="pin" maxlength="6" required>
+            <br>
             <small>PIN must be 6 digits.</small>
         </div>
 
