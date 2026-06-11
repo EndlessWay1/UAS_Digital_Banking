f59478716f12b0341ac85b97c49e85f26cbcd773
Nickson Kiyoshi <phoneasus158@gmail.com>
2026-06-11 11:48:06 +0700

    added css to user profile

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,35 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
-</head>
-<h1>User Profile</h1>
-@if (!$user){
-<h2>
-    An Error has Occurred
-</h2>
-}
-@endif
+    <x-slot:title>Profile</x-slot:title>
 
-<span style="font-size: medium;">Name: {{ $user->name }}</span><br>
-<span style="font-size: medium;">Email: {{ $user->email }}</span><br>
-<span style="font-size: medium;">Role: {{ $user->role ?? 'user'}}</span>
+    <div class="flex justify-center min-h-[calc(100vh-16rem)]">
+        <div class="card w-9/12 bg-base-100 ">
+            <div class="card-body">
+                <h1 class="text-3xl font-bold text-left">User Profile</h1>
+                <hr class="h-px my-2 bg-black border-0">
+                <div class="flex flex-col gap-3">
+                    <div class="flex flex-col gap-1">
+                        <span class="text-lg font-bold">Name: </span>
+                        <span class="rounded-lg bg-base-200 px-2 py-1 text-lg border">{{ $user->name }}</span>
+                    </div>
+                    <div class="flex flex-col gap-1">
+                        <span class="text-lg font-bold">Email: </span>
+                        <span class="rounded-lg bg-base-200 px-2 py-1 text-lg border">{{ $user->email }}</span>
+                    </div>
+                    <div class="flex flex-col gap-1">
+                        <span class="text-lg font-bold">Role: </span>
+                        <span class="rounded-lg bg-base-200 px-2 py-1 text-lg border">{{ $user->role ?? 'user' }}</span>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-4 justify-center mt-8">
+                    <a href="{{ route('profile.edit') }}" class='btn w-10vh'>Edit Profile</a>
 
-<form action="{{ route('profile.edit') }}" method="get">
-    @csrf
-    <button type="submit">Edit Profile</button>
-</form>
-<br>
-<form action="{{route('home')}}" method="get">
-    @csrf
-    <button type="submit">Home</button>
-</form>
+                    <a href="{{ route('home') }} " class="btn w-10vh">Home</a>
+                </div>
 
-<body>
+            </div>
+        </div>
+    </div>
 
-</body>
-
-</html>
+</x-layout>
