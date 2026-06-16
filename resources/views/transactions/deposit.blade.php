cf519670d39547631692fb71c9cbce73d2557fb1
vaibhav0629 <kapadiavaibhav19@gmail.com>
2026-06-16 11:27:04 +0700

    menghandle variabel bug di views deposit, transfer & withdraw

--- resources/views/transactions/deposit.blade.php
+++ resources/views/transactions/deposit.blade.php
@@ -7,23 +7,19 @@
 </head>
 <body>
     <h1>Deposit</h1>
-    <p>Welcome, {{ $user->name }}</p>
-
+    <p>Account: {{ $account->account_number }}</p>
     @if($errors->any())
         @foreach($errors->all() as $error)
             <p style="color:red">{{ $error }}</p>
         @endforeach
     @endif
-
     <form action="{{ route('transactions.deposit') }}" method="POST">
         @csrf
         <label for="amount">Amount (Rp):</label>
         <input type="number" id="amount" name="amount" min="1" required value="{{ old('amount') }}">
         <br><br>
-
         <button type="submit">Deposit</button>
     </form>
-
     <br>
     <a href="{{ route('transactions.index') }}">Back to Transaction History</a>
 </body>
