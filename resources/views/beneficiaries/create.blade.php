3b187fb653e1dafd5d6b954b32680aa5358edaf1
cathhzeee <cathyzll1504@gmail.com>
2026-06-21 15:34:44 +0700

    fix typo

--- resources/views/beneficiaries/create.blade.php
+++ resources/views/beneficiaries/create.blade.php
@@ -30,7 +30,7 @@
 
     @if ($errors->any())
         <ul>
-            @foreach (errors->all() as $error)
+            @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
         </ul>
